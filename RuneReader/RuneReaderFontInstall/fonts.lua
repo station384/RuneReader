2021df6ac5afbd53972fa82218eab81911a91153
Station <station384@gmail.com>
2024-08-27 17:18:06 -0400

    Update Fontloader

---
+++ RuneReader/RuneReaderFontInstall/fonts.lua
@@ -0,0 +1,10 @@
+local LSM = LibStub("LibSharedMedia-3.0")
+
+--Example
+-- LSM:Register("font", "YOUR FONT NAME", [[Interface\Addons\RuneReaderFontInstall\Fonts\YOUR-FONT-FILE-NAME.ttf]])
+
+--Fonts
+LSM:Register("font", "mitra-lt-bold", [[Interface\Addons\RuneReaderFontInstall\Fonts\ufonts.com_mitra-lt-bold.ttf]])
+LSM:Register("font", "Code39", [[Interface\Addons\RuneReaderFontInstall\Fonts\LibreBarcode39-Regular.ttf]])
+LSM:Register("font", "Code39Text", [[Interface\Addons\RuneReaderFontInstall\Fonts\LibreBarcode39Text-Regular.ttf]])
+UIErrorsFrame:Hide()
